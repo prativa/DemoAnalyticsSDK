534019a4dbe4edc66f5fe9dcefc3c7a160fcb34b
prativa neupane <prativananu123@gmail.com>
2024-06-24 17:20:03 +0545

    set up library for maven publish

--- mysdk/build.gradle.kts
+++ mysdk/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
     }
@@ -59,9 +59,9 @@ afterEvaluate {
     publishing {
         publications {
             register<MavenPublication>("release") {
-                groupId = "com.prativa"
-                artifactId = "analytics-sdk"
-                version = "1.0"
+               afterEvaluate {
+                   from(components["release"])
+               }
             }
         }
     }
